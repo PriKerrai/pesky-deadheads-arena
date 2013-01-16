a7ee9b86f423ec585bc0bae56a8856907f19bf93
s117902@student.hb.se <s117902@student.hb.se@821d25f7-7568-4cbe-0bd4-3d245310c0c0>
2013-01-16 13:45:35 +0000

    hej

---
+++ Arena/src/Tournament_2/Tournament.java
@@ -0,0 +1,30 @@
+
+package Tournament_2;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ * Representation of a tournament.
+ *
+ * @author Karl Sjöstrand
+ *
+ * To implement:
+ *
+ * Thoughts:
+ *
+ */
+public class Tournament 
+{
+    // Attributes
+    private ArrayList<PlayerId> playersInTournament = new ArrayList();
+    private Date startDate, endDate;
+    
+    // Constructors
+    public Tournament()
+    {
+        
+    }
+
+    // Methods
+}
